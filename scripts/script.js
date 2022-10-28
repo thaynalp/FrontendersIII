404b060267030d4cfee4a3d3c53dc670662b7f29
thaynalp <112887006+thaynalp@users.noreply.github.com>
2022-10-28 15:18:35 -0300

    js

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,21 @@ $(function(){
         if( email == '' || !er.test(email) ) { alert('Preencha o campo email corretamente'); return false; }
 
         // Se passou por essas validações exibe um alert
-        alert( 'formulário enviado com sucesso!' );
+        alert( 'Uma nova senha foi gerada e enviada para seu e-mail cadastrado!' );
+    });
+});
+
+// pág Login
+$(function(){
+    $('.form-login').submit(function(){
+        var er = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
+        var nome = $('#exampleInputEmail1').val();
+        var email = $('#exampleInputPassword1').val();
+
+        if( senha == '' ) { alert('Preencha o campo nome'); return false; }
+        if( email == '' || !er.test(email) ) { alert('Preencha o campo email corretamente'); return false; }
+
+        // Se passou por essas validações exibe um alert
+        alert( 'Uma nova senha foi gerada e enviada para seu e-mail cadastrado!' );
     });
 });
