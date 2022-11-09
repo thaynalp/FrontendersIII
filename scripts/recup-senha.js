027b87d6c3160436016e0ab3fbdc5c3edf51ee86
thaynalp <112887006+thaynalp@users.noreply.github.com>
2022-11-08 22:13:29 -0300

    recup senha up

--- scripts/recup-senha.js
+++ scripts/recup-senha.js
@@ -1,15 +1,16 @@
-// pág Login
+const form = document.querySelector("#form_contato")
+
 $(function(){
-    $('.form-login').submit(function(){
+    $('#form_contato').submit(function(){
         var er = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
-        var email = $('#exampleInputPassword1').val();
-        var senha = $('#senha').val();
+        var email = $('#txt_email').val();
+        // var senha = $('#senha').val();
 
         if( email == '' || !er.test(email) ) { alert('Preencha o campo email corretamente'); return false; }
-        if( senha == '' ) { alert('Preencha o campo nome'); return false; }
+        // if( senha == '' ) { alert('Preencha o campo nome'); return false; }
 
         // Se passou por essas validações exibe um alert
-        alert( 'ok' );
+        alert( 'Uma nova senha foi enviada para o e-mail cadastrado!' );
     });
 });
 
